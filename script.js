f79a59e3d622b9d92c42b0ec0895c68b5ed6dffd
dobatovkin <yahor.dabatoukin@mapbox.com>
2023-02-08 01:28:27 +0300

    final testing

---
+++ script.js
@@ -0,0 +1,7 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFiYXRvdWtpbiIsImEiOiJjbGFsY255N3AwNGQ2NDF0YWxuZHBmbjR1In0.AyxIdTiW3MUfAaHJgmO2iA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'https://dobatovkin.github.io/mbx-style/test-style.json'
+});
